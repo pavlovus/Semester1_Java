d74a072e39795b88497abc300a80d68abc2ff683
pavlovus <91273323+pavlovus@users.noreply.github.com>
2026-02-26 16:45:01 +0200

    Add files via upload

---
+++ pw1/Practice2/Practice2.java
@@ -0,0 +1,31 @@
+import stanford.karel.*;
+
+public class Practice2 extends Karel{
+
+	public void run(){
+		move();
+		pickBeeper();
+		turnLeft();
+		move();
+		move();
+		move();
+		move();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		move();
+		move();
+		putBeeper();
+		turnLeft();
+		turnLeft();
+		move();
+		move();
+		move();
+		turnLeft();
+		move();
+		move();
+		move();
+		move();
+		turnLeft();
+	}
+}
