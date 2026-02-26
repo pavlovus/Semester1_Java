f62895e9ebecf405223cc6d7f7554c1dd50226be
pavlovus <91273323+pavlovus@users.noreply.github.com>
2026-02-26 16:57:36 +0200

    Add files via upload

---
+++ hw1/Assignment1/FirstProgram.java
@@ -0,0 +1,71 @@
+/*
+ * Це заготовка для першого практичного зайняття
+ * світ має назву lection1
+ */
+import stanford.karel.*;
+
+public class FirstProgram extends Karel{
+
+	public void run(){
+		move();
+		pickBeeper();
+		move();
+		move();
+		move();
+		move();
+		move();
+		turnLeft();
+		move();
+		move();
+		move();
+		turnLeft();
+		move();
+		move();
+		turnLeft();
+		move();
+		turnLeft();
+		move();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		move();
+		putBeeper();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		move();
+		move();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		move();
+		move();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		move();
+		move();
+		move();
+		turnLeft();
+		move();
+		move();
+		turnLeft();
+		move();
+		move();
+		move();
+		move();
+		turnLeft();
+		move();
+		move();
+		move();
+		move();
+		move();
+		turnLeft();
+		turnLeft();
+		turnLeft();
+		move();
+		move();
+		turnLeft();
+		turnLeft();
+	}
+}
